98858eb31c9b0b696826b9e1a43feede8e76ce40
karlmartin.miidu <karlmartin.miidu@transferwise.com>
2023-05-01 20:57:42 +0300

    - bootstrap

---
+++ js/commands.js
@@ -0,0 +1,121 @@
+const EMAIL = 'karlmartin.miidu@gmail.com';
+
+
+const ABOUT = [
+  "<br>",
+  "Welcome to Miidu OS 1.0!",
+  "<br>",
+  "<br>",
+  "<br>",
+  "I'm Karl, senior software engineering lead ",
+  "with 10+ years of professional experience. ",
+  "I’ve been fascinated by computers ever since I was a child ",
+  "and I consider myself lucky that I can still spend my days ",
+  "doing work for which I’m genuinely passionate about.",
+  "<br>",
+];
+
+
+
+PROJECTS = [
+  "<br>",
+  "Here are some of my projects:",
+  "<br>",
+  "<strong> - MINDy App <strong>",
+  "Mental health startup using " +
+  "Cognitive Behavioural Therapy " +
+  "to help people with depression and anxiety.",
+  "Tech stack: React Native, Java 18, AWS",
+  "<br>",
+  "<strong> - E-Res Network <strong>",
+  "First social network with government " +
+  "backed user verification (e-residency) " +
+  "Tech stack: PHP, MySQL",
+  "<br>",
+];
+
+COMMAND_LIST = [
+  "<br>",
+  '<span class="command">about</span>          Learn more about me',
+  '<span class="command">education</span>      Education',
+  '<span class="command">career</span>         Professional experience',
+  '<span class="command">projects</span>       View some of my projects',
+  '<span class="command">history</span>        Show command history',
+  '<span class="command">email</span>          Feel free to reach out',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">echo</span>           Print any give string',
+  '<span class="command">help</span>           Display all commands',
+  "<br>",
+];
+
+BANNER = [
+    " "
+];
+
+EMAIL_INFO = [
+  "                 ",
+  "✉️ " + "<a href=mailto:" + EMAIL + ">" + EMAIL +"</a>",
+  "                  "
+]
+
+SUDO = [
+"                                                                ",
+"Only Karl has <span class=\"command\">admin</span> privileges :(",
+"                                                                "
+]
+
+WORK_INFO = [
+  "<br><strong>WISE LTD - Senior Engineering Lead</strong>",
+  "2022 - ",
+  "Responsible for the FinCrime tribe with 4 engineering teams, 25+ engineers in total." ,
+  "Financial Fraud Detection”",
+  "<br>",
+  "<strong>WISE LTD - Software Engineer, Engineering Lead</strong>",
+  "2017 – 2022",
+  "Developed Fraud detection system capable of checking near real-time ",
+  "(every payment regardless of the load gets a live ML prediction with updated data under 10 seconds).",
+  "Technologies: Kafka, Kafka Streams, Postgres, Kubernetes, Java 18, Snowflake",
+  "<br>",
+  "<strong>TIETO ESTONIA AS - Software Engineer</strong>",
+  "2016 – 2017",
+  "Developed a new Estonian Social Security Information System",
+  "<br>",
+  "<strong>SCORO SOFTWARE - Junior Software Engineer</strong>",
+  "2014 – 2016",
+  "CRM system development",
+  "<br>"
+]
+
+EDUCATION_INFO = [
+  "<br><strong>MSc. Business Information Technology</strong>",
+  "Tallinn University of Technology  |  2017 - 2020",
+  "Thesis: “A Comparison between a SQL Database and a Graph Database in the context of \n" +
+  "Financial Fraud Detection”\n",
+  "                                                  ",
+  "<strong>BSc. Computer Science</strong>",
+  "Tallinn University of Technology  |  2013 – 2016",
+  "Thesis: ”Estonian Emergency system on mobile - live video stream from phone to emergency center”",
+  "                                                  ",
+  "<br>"
+]
+
+TERMINAL_INFO = [
+'' +
+'' +
+'' +
+'Welcome to Miidu OS 1.0',
+"For a list of available commands, type <span class=\"command\">'help'</span>."
+]
+
+TERMINAL_INFO_MOBILE = [
+  '' +
+  '' +
+  '' +
+  'Welcome to Miidu OS 1.0!',
+  "                                 ",
+  "For a list of available commands,", 
+  "type <span class=\"command\">'help'</span>.",
+  "                                          ",
+  "Version (1.0)",
+  "                                          "
+  ]
